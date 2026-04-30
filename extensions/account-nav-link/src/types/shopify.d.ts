ed5c69da1c082df257bb6f21522bcf0faf65b1d1
deepak486 <dpkverma486@gmail.com>
2026-04-30 23:58:42 +0530

    Invoice Tab WIP

---
+++ extensions/account-nav-link/src/types/shopify.d.ts
@@ -0,0 +1,29 @@
+declare namespace preact.JSX {
+  interface IntrinsicElements {
+    // Layout
+    's-page': any;
+    's-card': any;
+    's-stack': any;
+    's-divider': any;
+
+    // Typography
+    's-text': any;
+    's-heading': any;
+    's-link': any;
+
+    // Actions
+    's-button': any;
+
+    // Forms
+    's-text-field': any;
+
+    // Feedback
+    's-banner': any;
+
+    // Table
+    's-table': any;
+    's-table-row': any;
+    's-table-header-cell': any;
+    's-table-cell': any;
+  }
+}
